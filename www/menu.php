6e2b49dd23d1326d9d3a4bd97d996aa4184b6bb1
arc680 <maru.nslab@gmail.com>
2015-05-08 15:32:25 +0900

    Remove link to old blog

--- www/menu.php
+++ www/menu.php
@@ -3,11 +3,8 @@ My Accounts
 <ul>
     <li><a href="http://twitter.com/arc680">Twitter</a></li>
     <li><a href="http://github.com/arc680/">Github</a></li>
-    <li><a href="http://github.com/arc680/">Gist</a></li>
+    <li><a href="https://gist.github.com/arc680">Gist</a></li>
     <li><a href="http://arc680.hatenablog.jp/">ブログ</a></li>
-    <li><a href="http://d.hatena.ne.jp/arc680/">旧ブログ</a></li>
-    <li><a href="http://jubegraph.dyndns.org/jubeat_saucer/score.cgi?rid=57710013624746">jubegraph</a></li>
-    <li><a href="http://bot.arc680.info/">( っ'ω'c)bot</a></li>
 </ul>
 勝手なリンク
 <ul>
